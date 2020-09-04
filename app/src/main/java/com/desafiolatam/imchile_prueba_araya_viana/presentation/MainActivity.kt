be8093aada768bccc52dcff24086400a05c3b9fd
eklessia <amplificacion@mail.com>
2020-09-04 17:10:02 -0400

    Aqui vamos

---
+++ app/src/main/java/com/desafiolatam/imchile_prueba_araya_viana/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.desafiolatam.imchile_prueba_araya_viana.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.desafiolatam.imchile_prueba_araya_viana.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
